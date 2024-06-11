bb50a388781735249440f7885d5353460dc5d10d
Jotas19 <juan197692@gmail.com>
2024-06-11 07:14:42 -0500

    Inicialización de la BD (Error con MIME)

--- src/components/utils/Firebase.js
+++ src/components/utils/Firebase.js
@@ -1,14 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAOkVuBT8sfvSuJarFLEqPtukZfJdkkEVg",
-  authDomain: "patrones-6d287.firebaseapp.com",
-  projectId: "patrones-6d287",
-  storageBucket: "patrones-6d287.appspot.com",
-  messagingSenderId: "613813973576",
-  appId: "1:613813973576:web:d0a6d42956fb2c8afcde7d"
+  apiKey: "AIzaSyCUOXjRZhHxqXUhBDHXr9HsHCeuszfKpyM",
+  authDomain: "patrones-95d74.firebaseapp.com",
+  databaseURL: "https://patrones-95d74-default-rtdb.firebaseio.com",
+  projectId: "patrones-95d74",
+  storageBucket: "patrones-95d74.appspot.com",
+  messagingSenderId: "699099451650",
+  appId: "1:699099451650:web:d25df3ccfebf739176721b",
+  measurementId: "G-ECM8GGDYLX"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-console.log(app)
+const analytics = getAnalytics(app);
